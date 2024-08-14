a53be1ea4a89147808798eecd92d8d27c8e34a8c
Mashanz <mangatkk@gmail.com>
2024-08-14 20:27:20 +0700

    feat: menambahkan fitur B

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+fn fitur_b() {
+    print!("Fitur B");
+}
+
 fn fitur_a() {
     println!("Fitur A")
 }
@@ -5,5 +9,6 @@ fn fitur_a() {
 fn main() {
     let n: u8 = 5;
     println!("Halo Dunia! {}", n);
+    fitur_b();
     fitur_a();
 }
