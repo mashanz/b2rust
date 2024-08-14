64f1902053f6334f57cdf1f80850702b8ef4c420
Mashanz <mangatkk@gmail.com>
2024-08-14 21:05:28 +0700

    feat: fitur y

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ fn fitur_x() {
     println!("fitur X");
 }
 
+fn fitur_y() {
+    println!("fitur y");
+}
+
 fn main() {
     let n: u8 = 5;
     println!("Halo Dunia! {}", n);
     fitur_x();
+    fitur_y();
 }
